d5ff2bd3d13650c463bbcf452797c001fcd2274b
TrentREis <133795091+TrentREis@users.noreply.github.com>
2025-05-22 13:07:45 -0400

    Update App.js

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -7,7 +7,7 @@ import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 
 function App() {
-   const navigate = useNavigate(); // Silence unused-var warning
+  
 
 
   return (
